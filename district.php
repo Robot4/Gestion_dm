cba90301ee6991db6751d5158734ee34a90e0d4d
mehdi <mehdi.hail.33@gmail.com>
2023-07-13 13:02:52 +0100

    here

--- district.php
+++ district.php
@@ -1,10 +1,10 @@
 <?php
 require('rabat.php');
 
-// Check if the user is logged in and authorized to access district.php
-if (!isset($_SESSION["username"]) || $_SESSION["usertype"] !== "user") {
+
+if(!isset($_SESSION["username"]))
+{
     header("location:login.php");
-    exit();
 }
 ?>
 
@@ -15,23 +15,9 @@ if (!isset($_SESSION["username"]) || $_SESSION["usertype"] !== "user") {
 </head>
 <body>
 
+
 <a href="logout.php">Logout</a>
 <a href="profile.php">Profile</a>
-
-<?php
-// Check if the user has access to the menu items in the district
-if ($_SESSION["access_district"]) {
-    ?>
-    <!-- Display the specific menu items for the user -->
-    <ul>
-        <li>Gestion DM</li>
-        <li>Gestion Stock</li>
-        <li>Gestion d'outillage</li>
-        <li>Valorisation</li>
-    </ul>
-    <?php
-}
-?>
 
 </body>
 </html>
